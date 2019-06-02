9a0002d017c9963cbe381dc4590f52cc77d2def7
Valen. H <alternativexxxy@gmail.com>
2019-06-02 13:03:39 +0300

    Added help screen and manpage

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
-﻿
+﻿  
 # DirStamp  
+  
 Take stamps of directories with ease!  
   
 ## Usage  
-`dirstamp path/`  
-`dirstamp path/ -j > struct.json`  
+  
+* `dirstamp path/`  
+* `dirstamp path/ -j > struct.json`  
   
 > Validate current structure out of `.json` struct files with `dirstamp -c struct.json path/`  
 
 ## Options  
-**-p** -- Switch sorting method from `shift` to `push`  
-**-j** -- Print JSON  
-**-s** -- Follow Symlinks  
-**-d** -- Max depth (Defaults to Infinity [-1] )  
-**-c** -- Read a JSON file and compare to current structure  
+  
+* **-p** -- Switch sorting method from `shift` to `push`  
+* **-j** -- Print JSON  
+* **-s** -- Follow Symlinks  
+* **-d \<Number>** -- Max depth (Defaults to Infinity [-1] )  
+* **-c \<File>** -- Read a JSON file and compare to current structure  
+* **-C** -- Disable colors  
+* **-w** -- Override default behaviour and start watching changes instead (ctrl+c to stop) ['rename' might be equal to add/delete]  
+* **-h** -- Help screen  
+  
--- out/dirstamp.js
+++ out/dirstamp.js
@@ -6,42 +6,39 @@ const path = require("path");
 const fs = require("fs");
 const chalk = require("chalk");
 const os_1 = require("os");
+const strip = require("strip-ansi");
 const opts = {
     depthLimit: -1,
     preserveSymlinks: false,
     queueMethod: "shift"
 }, custom = {
     json: false,
-    check: null
+    check: null,
+    watch: false,
+    nocolors: false
 };
-function remove(arr, rem) {
-    let idx = arr.findIndex((v) => v === rem);
-    if (idx < 0)
-        return;
-    return arr.splice(idx, 1);
-} //remove
-function ensure(pth) {
-    let pths = pth.split(path.sep).slice(0, -1);
-    let obj = struct, par = struct;
+function ensure(pt) {
+    pt.path = path.relative(path.resolve(pth), pt.path);
+    let pths = pt.path.split(path.sep).slice(0, -1), obj = struct, par = struct;
     for (var i of pths) {
         par = obj;
         obj = (obj[i] = obj[i] || {});
     }
-    if (i) {
+    if (i && par[i]) {
         par[i] = (par[i] = par[i] || {});
-        par[i][pth.split(path.sep).pop()] = '';
+        par[i][pt.path.split(path.sep).pop()] = pt.stats.isDirectory() ? {} : '';
     }
     else {
-        par[pth.split(path.sep).pop()] = '';
+        par[pt.path.split(path.sep).pop()] = pt.stats.isDirectory() ? {} : '';
     }
 } //ensure
 function breakdown(root = struct, offset = 0, key) {
-    let tb = "\t".repeat(offset), ret = os_1.EOL + tb + key;
-    tb += '\t';
+    let tb = '\t'.repeat(offset), ret = os_1.EOL;
     if (typeof root === "string") {
-        ret += tb + root;
+        ret += custom.nocolors ? tb + key : chalk["cyan"]["dim"](tb + key);
     }
     else {
+        ret += custom.nocolors ? tb + key : chalk[strip(key).trim().endsWith("G:") ? "red" : (strip(key).trim().endsWith("S:") ? "green" : "yellow")]["dim"](tb + key);
         for (let i in root) {
             ret += breakdown(root[i], offset + 1, i);
         }
@@ -53,7 +50,7 @@ function checkValidity(struct, base = check, missing = {}) {
     for (let i in struct) {
         if (typeof struct[i] === "string") {
             if (!(i in base)) {
-                missing[i] = '';
+                missing[i] = struct[i];
             }
             delete base[i];
         }
@@ -78,9 +75,15 @@ function prune(prop, targ) {
     return targ;
 } //prune
 let copy = Array.from(process.argv.slice(2)), struct = {}, pth = process.cwd(), check = {};
-opts.preserveSymlinks = copy.includes("-s");
-custom.json = copy.includes("-j");
-opts.queueMethod = (copy.includes("-p") ? "pop" : "shift");
+if (copy.includes("-h")) {
+    console.info("Run 'man dirstamp'.");
+    process.exit(0);
+}
+opts.preserveSymlinks = /-.*s/.test(copy.join(' '));
+custom.json = /-.*j/.test(copy.join(' '));
+(custom.nocolors = /-.*C/.test(copy.join(' '))) && (chalk["level"] = 0);
+custom.watch = /-.*w/.test(copy.join(' '));
+opts.queueMethod = (/-.*p/.test(copy.join(' ')) ? "pop" : "shift");
 if (copy.includes("-d")) {
     let idx;
     opts.depthLimit = Number.parseInt(copy[(idx = copy.findIndex((val) => val === "-d")) + 1]);
@@ -97,32 +100,44 @@ if (copy.includes("-c")) {
         check = JSON.parse(fs.readFileSync(custom.check, "utf8"));
     }
     catch (err) {
-        console.error(chalk["red"](err.message));
+        console.error(chalk["redBright"](err.message));
         process.exit(err.code);
     }
     copy.splice(idx, 2);
 }
-remove(copy, "-p");
-remove(copy, "-s");
-remove(copy, "-j");
-klaw(pth = path.normalize(copy.join(' ') || pth), opts)
-    .on("data", (item) => {
-    ensure(path.relative(path.normalize(pth), item.path));
-}).once("end", () => {
-    delete struct[""];
-    if (custom.check) {
-        let out = checkValidity(struct);
-        //@ts-ignore
-        process.stdout.write(chalk["green"](breakdown(out[0].missing, 0, chalk["underline"]("EXTRANEOUS:") + os_1.EOL)));
-        process.stdout.write(chalk["red"](breakdown(out[1], 0, chalk["underline"]("MISSING:") + os_1.EOL)) + os_1.EOL);
-    }
-    else if (custom.json) {
-        process.stdout.write(JSON.stringify(struct, null, 0));
-    }
-    else {
-        process.stdout.write(breakdown(struct, 0, path.basename(pth)));
-    }
-}).once("error", (err) => {
-    console.error(chalk["red"](err.message));
-});
+/*
+ * RESERVED:
+ * -p, -s, -j, -C, -c, -w, -d, -h
+ */
+copy = copy.filter((v) => !v.startsWith('-'));
+if (custom.watch) {
+    fs.watch(pth, {
+        persistent: true,
+        recursive: true
+    }, (evt, file) => {
+        console.log(chalk["bold"](evt), chalk["underline"](file), chalk["gray"](Date()));
+    });
+}
+else {
+    klaw(pth = copy.join(' ') || pth, opts)
+        .on("data", (item) => {
+        ensure(item);
+    }).once("end", () => {
+        delete struct[''];
+        if (custom.check) {
+            let out = checkValidity(struct);
+            //@ts-ignore
+            process.stdout.write(breakdown(out[0].missing, 0, chalk["underline"]("EXTRANEOUS:") + os_1.EOL));
+            process.stdout.write(breakdown(out[1], 0, chalk["underline"]("MISSING:") + os_1.EOL) + os_1.EOL);
+        }
+        else if (custom.json) {
+            process.stdout.write(JSON.stringify(struct, null, 0));
+        }
+        else {
+            process.stdout.write(breakdown(struct, 0, path.basename(pth)) + os_1.EOL);
+        }
+    }).once("error", (err) => {
+        console.error(chalk["red"](err.message));
+    });
+}
 //# sourceMappingURL=dirstamp.js.map
